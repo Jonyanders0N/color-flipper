0b8c1bc030adc3dbbbcb5d0e3885e9911bff887e
Jonyanderson <sonyanderson19@gmail.com>
2024-02-26 21:28:30 +0000

    ajust layout

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-colors = ['green', 'red', 'rgba(133,122,200', '#f15025'];
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
 const btn = document.getElementById('btn');
 const color = document.querySelector('.color');
 
-btn.addEventListener('click', function() {
-    const randomNumber = getRandomNumber();
-    console.log(randomNumber);
+btn.addEventListener('click', () => {
+    const stringRandom = createHexString();
+    // console.log(stringRandom);
 
-    document.body.style.backgroundColor = colors[randomNumber];
-    color.textContent = colors[randomNumber];
-});
+    document.body.style.backgroundColor = stringRandom;
+    color.textContent = stringRandom;
+})
 
-function getRandomNumber(){
-    return Math.trunc(Math.random() * colors.length);
+function getNumberRandom() {
+    return Math.floor(Math.random() * hex.length);
 }
 
+function createHexString() {
+    let hexColor = '#';
+    for (let i = 0; i < 6; i++) {
+        var valueTemp = getNumberRandom()
+        hexColor = hexColor.concat(hex[valueTemp]);
+    }
+    return hexColor;
+}
